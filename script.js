1d36f14b4bf5920bc16b254bd13802cc292e1e60
curlyheadcoder <mayanksonwani078@gmail.com>
2025-05-14 01:04:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ function convertToRoman(num) {
 		return res;
 
 }
-You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
+// console.log(convertToRoman(36));
 
 
 
